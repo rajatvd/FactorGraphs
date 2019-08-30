89617ad5921c922789ed2f36bf6cc2577edaf424
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2019-08-30 22:28:19 +0530

    added better factor graph nodes and edges

--- factor_graph.py
+++ factor_graph.py
@@ -8,7 +8,7 @@ import string
 
 
 def factor_graph(factors, einpath):
-    fg = nx.Graph()  # make graph
+    fg = nx.MultiGraph()  # make graph
 
     for f_name, f in factors.items():  # add factors
         fg.add_node(f_name, factor=f, type='factor')
@@ -40,39 +40,99 @@ def factor_graph(factors, einpath):
 
 
 def get_fg_node(n, G):
-    nname = n
-    n = G.node[n]
+    node = G.node[n]
     type_to_shape = {
         'variable': Circle,
         'factor': Square
     }
 
-    def node_color(node):
-        if node['type'] == 'factor':
+    def node_color(node_dict):
+        if node_dict['type'] == 'factor':
             return GREEN
-        return WHITE if not node['summed'] else GRAY
-    bg = type_to_shape[n['type']](color=BLACK, fill_color=node_color(n),
-                                  fill_opacity=1, radius=0.3, side_length=0.8)
-    node_name = TextMobject(nname, color=BLACK)
-    x, y = n['pos']
-    node_name.move_to(x*RIGHT + y*UP)
-    bg.move_to(x*RIGHT + y*UP)
-    return VGroup(bg, node_name)
+        return WHITE if not node_dict['summed'] else GRAY
 
+    bg = type_to_shape[node['type']](color=BLACK, fill_color=node_color(node),
+                                     fill_opacity=1, radius=0.3, side_length=0.8)
+    node_name = TextMobject(n, color=BLACK)
+    x, y = node['pos']
+    grp = VGroup(bg, node_name)
+    grp.move_to(x*RIGHT + y*UP)
+    return grp
 
 # %%
-def get_fg_edge(n1, n2, G):
+
+
+def get_fg_edge_line(n1, n2, G):
     s = G[n1][n2]['size']
-    n1 = G.node[n1]
-    n2 = G.node[n2]
-    x1, y1 = n1['pos']
-    x2, y2 = n2['pos']
+    node1 = G.node[n1]
+    node2 = G.node[n2]
+    x1, y1 = node1['pos']
+    x2, y2 = node2['pos']
     start = x1*RIGHT + y1*UP
     end = x2*RIGHT + y2*UP
-    return Line(start, end, stroke_width=s/10, color=BLACK)
-
+    edge = Line(start, end, stroke_width=s/10, color=BLACK)
+    return edge
 
 # %%
+
+
+def get_closest_polygonal_point(n1, pos, G):
+    deg = G.degree[n1]
+    node = G.node[n1]
+    phase = node.get('phase', 0)
+    rad = node.get('radius', 0.4)
+    pos = np.array(pos)
+    centre = np.array(node['pos'])
+    thetas = np.arange(deg) * 2*np.pi/deg + phase
+    polyg = np.stack([np.cos(thetas), np.sin(thetas)], axis=-1)
+    points = centre + polyg*rad
+    dists = np.linalg.norm(points-pos, axis=1)
+    return np.array(list(points[dists.argmin()])+[0])
+
+# %%
+
+
+def get_fg_edge_polygonal(n1, n2, G):
+    s = G[n1][n2]['size']
+    node1 = G.node[n1]
+    node2 = G.node[n2]
+    x1, y1 = node1['pos']
+    x2, y2 = node2['pos']
+    start = x1*RIGHT + y1*UP
+    end = x2*RIGHT + y2*UP
+
+    p1 = get_closest_polygonal_point(n1, (x2, y2), G)
+    p2 = get_closest_polygonal_point(n2, (x1, y1), G)
+
+    edge = VMobject(stroke_width=s/10, color=BLACK)
+    edge.set_points_smoothly([start, p1, p2, end])
+
+    # Line(start, end, stroke_width=s/10, color=BLACK)
+
+    return edge
+
+# %%
+
+
+def get_fg_edge_curve(n1, n2, k, G):
+    s = G[n1][n2][k]['size']
+    node1 = G.node[n1]
+    node2 = G.node[n2]
+    x1, y1 = node1['pos']
+    x2, y2 = node2['pos']
+    start = x1*RIGHT + y1*UP
+    end = x2*RIGHT + y2*UP
+
+    pnts = [x*RIGHT + y*UP for x, y in G[n1][n2][k].get('points', [])]
+
+    edge = VMobject(stroke_width=s/10, color=BLACK)
+    edge.set_points_smoothly([start, *pnts, end])
+
+    return edge
+
+# %%
+
+
 class Test(Scene):
     def construct(self):
         np.random.seed()
@@ -82,18 +142,18 @@ class Test(Scene):
         D = np.random.randn(10, 100, 50)
         E = np.random.randn(20)
         factors = {
-            'A': A,
-            'B': B,
-            'C': C,
+            # 'A': A,
+            # 'B': B,
+            # 'C': C,
             'D': D,
-            'E': E,
+            # 'E': E,
         }
 
-        einpath = 'xu,uy,uv,zwv,y -> xyzw'
+        einpath = 'iik -> ik'
 
         fg = factor_graph(factors, einpath)
 
-        mfg = mnx.ManimGraph(fg, get_fg_node, get_fg_edge)
+        mfg = mnx.ManimGraph(fg, get_fg_node, get_fg_edge_curve)
 
         self.play(ShowCreation(mfg))
         self.wait(2)
