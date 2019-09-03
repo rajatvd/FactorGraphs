df894ac1edd989e20beb73932f3a6404a0e2a30a
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2019-09-03 10:58:29 +0530

    implemented removals and contractions in transform graph

--- fg_anim.py
+++ fg_anim.py
@@ -57,9 +57,14 @@ def get_fg_edge_curve(ed, G):
     return edge
 
 
-# %%
+# %% TODO: take in custom transform for edges and nodes
 def transform_graph(mng, G):
     """Transforms the graph in ManimGraph mng to the graph G.
+
+    For any new mob_ids in G, or any nodes or edges without a mob_id, they
+    are assumed to be new objects which are to be created. If they contain
+    an expansion attribute, they will be created and transformed from those
+    mob_ids in the expansion. Otherwise, they will be faded in.
 
     Missing mob_ids in G are dealt with in two ways:
     If they are found in the contraction attribute of the node or edge, the
@@ -68,10 +73,9 @@ def transform_graph(mng, G):
     that any older contractions present in G do not affect the animation in any
     way.)
 
-    For any new mob_ids in G, or any nodes or edges without a mob_id, they
-    are assumed to be new objects which are to be created. If they contain
-    an expansion attribute, they will be created and transformed from those
-    mob_ids in the expansion. Otherwise, they will be faded in.
+    Cannot contract to new nodes/edges and expand from new nodes/edges.
+    # TODO: consider changing this behavior
+
 
     Parameters
     ----------
@@ -86,10 +90,11 @@ def transform_graph(mng, G):
         List of animations to show the transform.
 
     """
-    old_ids = list(mng.nodes.keys()) + list(mng.edges.keys())
-
     anims = []
     id_to_mobj = {**mng.nodes, **mng.edges}
+
+    old_ids = list(mng.nodes.keys()) + list(mng.edges.keys())
+    new_ids = []
 
     # ------- ADDITIONS AND DIRECT TRANSFORMS ---------
     # NODES
@@ -100,11 +105,11 @@ def transform_graph(mng, G):
             mng.count += 1
 
         mob_id = node_data['mob_id']
+        new_ids.append(mob_id)
 
         if mob_id in old_ids:
-            # only transform if new is different from old
-            if mng.graph.nodes[mng.id_to_node[mob_id]] != node_data:
-                anims.append(Transform(mng.nodes[mob_id], new_node))
+            # if mng.graph.nodes[mng.id_to_node[mob_id]] != node_data:
+            anims.append(Transform(mng.nodes[mob_id], new_node))
         else:
             if 'expansion' in node_data.keys():
                 objs = [id_to_mobj[o['mob_id']]
@@ -125,11 +130,14 @@ def transform_graph(mng, G):
             mng.count += 1
 
         mob_id = edge_data['mob_id']
+        new_ids.append(mob_id)
 
         if mob_id in old_ids:
             # only transform if new is different from old
-            if mng.graph.edges[mng.id_to_edge[mob_id]] != edge_data:
-                anims.append(Transform(mng.edges[mob_id], new_edge))
+            # TODO: how to check this properly, and is it really needed?
+
+            # if mng.graph.edges[mng.id_to_edge[mob_id]] != edge_data:
+            anims.append(Transform(mng.edges[mob_id], new_edge))
         else:
             if 'expansion' in edge_data.keys():
                 objs = [id_to_mobj[o['mob_id']]
@@ -143,9 +151,51 @@ def transform_graph(mng, G):
             mng.id_to_edge[mob_id] = edge
 
     # --------- REMOVALS AND CONTRACTIONS ----------
-    # NODES TODO
+    # NODES
+    for node, node_data in mng.graph.nodes.items():
+        mob_id = node_data['mob_id']
+        if mob_id in new_ids:
+            continue
 
-    # EDGES TODO
+        contracts_to = []
+        for node2, node_data2 in G.nodes.items():
+            for c2 in node_data2.get('contraction', {}).values():
+                if mob_id == c2['mob_id']:
+                    contracts_to.append(id_to_mobj[node_data2['mob_id']])
+                    break
+
+        mobj = id_to_mobj[mob_id]
+        if len(contracts_to) == 0:
+            anims.append(FadeOut(mobj))
+        else:
+            anims.append(Transform(mobj, VGroup(*contracts_to)))
+
+        mng.remove(mobj)
+        del mng.nodes[mob_id]
+        del mng.id_to_node[mob_id]
+
+    # EDGES
+    for edge, edge_data in mng.graph.edges.items():
+        mob_id = edge_data['mob_id']
+        if mob_id in new_ids:
+            continue
+
+        contracts_to = []
+        for edge2, edge_data2 in G.edges.items():
+            for c2 in edge_data2.get('contraction', {}).values():
+                if mob_id == c2['mob_id']:
+                    contracts_to.append(id_to_mobj[edge_data2['mob_id']])
+                    break
+
+        mobj = id_to_mobj[mob_id]
+        if len(contracts_to) == 0:
+            anims.append(FadeOut(mobj))
+        else:
+            anims.append(Transform(mobj, VGroup(*contracts_to)))
+
+        mng.remove(mobj)
+        del mng.edges[mob_id]
+        del mng.id_to_edge[mob_id]
 
     mng.graph = G
     return anims
