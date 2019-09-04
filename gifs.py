2f665568fa8a8e116db80a52d22dc0a8bd74dc78
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2019-09-05 00:13:45 +0530

    stubs for gifs

---
+++ gifs.py
@@ -0,0 +1,54 @@
+from manimlib.imports import *
+from manimnx import manimnx as mnx
+from factor_graph import *
+from fg_anim import *
+import numpy as np
+import networkx as nx
+
+
+# %%
+class MatVec(Scene):
+    def construct(self):
+        pass
+
+
+# %%
+class MatMul(Scene):
+    def construct(self):
+        pass
+
+
+# %%
+class Hadamard(Scene):
+    def construct(self):
+        pass
+
+
+# %%
+class FakeReshape(Scene):
+    def construct(self):
+        pass
+
+
+# %%
+class TraceCyclic(Scene):
+    def construct(self):
+        pass
+
+
+# %%
+class MatrixInnerProduct(Scene):
+    def construct(self):
+        pass
+
+
+# %%
+class KronProperty(Scene):
+    def construct(self):
+        pass
+
+
+# %%
+class SVD(Scene):
+    def construct(self):
+        pass
