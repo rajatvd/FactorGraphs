d6fc9931d79a0d9171c70db6fb57696d3859bed9
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2019-09-08 09:46:48 +0530

    added mat vec gif

--- gifs.py
+++ gifs.py
@@ -1,15 +1,45 @@
 from manimlib.imports import *
 from manimnx import manimnx as mnx
-from factor_graph import *
-from fg_anim import *
 import numpy as np
 import networkx as nx
+import sys
+sys.path.append('.')
+if True:
+    from factor_graph import *
+    from fg_anim import *
 
 
 # %%
 class MatVec(Scene):
     def construct(self):
-        pass
+
+        A = np.random.randn(30, 60)
+        B = np.random.randn(60)
+        factors = {'A': A, 'B': B}
+        fg = factor_graph(factors, 'ij,k->ijk')
+
+        h = 2.5
+        pos = np.zeros((5, 2))
+        pos[:, 0] = (np.arange(5)-2)*h
+        mnx.map_attr('pos', ['i', 'A', 'j', 'k', 'B'], pos, fg)
+
+        mng = mnx.ManimGraph(fg, get_fg_node, get_fg_edge_curve)
+
+        self.add(mng)
+        self.wait(2)
+
+        fg = combine_variables('j', 'k', fg)
+
+        pos = np.zeros((4, 2))
+        pos[:, 0] = (np.arange(4)-1.5)*h
+        mnx.map_attr('pos', ['i', 'A', 'j', 'B'], pos, fg)
+        self.play(*mnx.transform_graph(mng, fg))
+
+        fg = fg.copy()
+        fg.nodes['j']['summed'] = True
+        self.play(*mnx.transform_graph(mng, fg))
+
+        self.wait(2)
 
 
 # %%
